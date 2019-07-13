6f057254eacd6ddb602b48186066b68dbad0b022
Zachary Williams <zwilliams.dev@gmail.com>
2019-07-13 01:29:57 -0400

    Removed cooldown code, kick.js issues need investigation

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,18 @@
 module.exports = {
   name: 'kick',
   description: 'Kick a user from the server',
-  args: true, // No arguments expected
+  args: true, // Arguments expected
   usage: '@username',
   guildOnly: true, // Will not work in a DM
   cooldown: 0,
   execute (message, args) {
-    const mentioned = message.mentions.users.first()
-    message.channel.send(`[DEBUG]: This would have kicked ${mentioned.username}`)
+    // Person to kick
+    const mentioned = message.mentions.members.first() || message.guild.members.get(args[0])
+    const reason = args.join(' ').slice(1)
+
+    // Check if person running command has perms
+    if (!message.member.hasPermission('KICK_MEMBERS')) {
+      return message.channel.send(`${message.author.username} who do you think you are trying to kick people....`)
+    }
   }
 }
